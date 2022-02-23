8d4d4a81f4ed196d05cb61dc4445fdde783f7aaa
VibeDylan <dylanbourbotte62@icloud.com>
2022-02-23 05:29:16 -0500

    Add passport-jwt and types/passport-jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
+import { Tokens } from './types';
 
 @Controller('auth')
 export class AuthController {
@@ -8,7 +9,7 @@ export class AuthController {
     constructor(private authService: AuthService) { }
 
     @Post('/local/signup')
-    signUpLocal(@Body() dto: AuthDto) {
+    signUpLocal(@Body() dto: AuthDto): Promise<Tokens> {
         this.authService.signUpLocal(dto)
     }
 
