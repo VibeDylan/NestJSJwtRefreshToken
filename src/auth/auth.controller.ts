f647dc640b0092721b842df6235366b5d19330fd
VibeDylan <dylanbourbotte62@icloud.com>
2022-02-23 09:26:14 -0500

    add httpcode decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Tokens } from './types';
@@ -9,21 +9,23 @@ export class AuthController {
     constructor(private authService: AuthService) { }
 
     @Post('/local/signup')
+    @HttpCode(HttpStatus.CREATED)
     signUpLocal(@Body() dto: AuthDto): Promise<Tokens> {
         return this.authService.signUpLocal(dto)
     }
 
     @Post('/local/signin')
+    @HttpCode(HttpStatus.OK)
     signInLocal(@Body() dto: AuthDto): Promise<Tokens> {
         return this.authService.signInLocal(dto)
     }
 
-    @Post('/logout')
+    @Post('logout')
     logout() {
-        this.authService.logout()
+        // return this.authService.logout()
     }
 
-    @Post('/refresh')
+    @Post('refresh')
     refreshTokens() {
         this.authService.refreshTokens()
     }
