a388cceabe79ccd832380c4dd995e381a7d5ee21
VibeDylan <dylanbourbotte62@icloud.com>
2022-02-23 05:03:32 -0500

    Create method for the route, in auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,29 @@
 import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
 
+    constructor(private authService: AuthService) { }
+
     @Post('/local/signup')
     signUpLocal() {
-
+        this.authService.signUpLocal()
     }
 
     @Post('/local/signin')
     signInLocal() {
-        
+        this.authService.signInLocal()
     }
 
     @Post('/logout')
     logout() {
-        
+        this.authService.logout()
     }
 
     @Post('/refresh')
     refreshTokens() {
-        
+        this.authService.refreshTokens()
     }
 
 }
